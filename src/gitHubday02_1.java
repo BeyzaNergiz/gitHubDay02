d7da2f1cae7a3d7abae7491b465fd23e7b1bfc22
Beyza Nergiz <byz.nergiz34@gmail.com>
2023-07-31 20:32:11 +0300

    ikinci satir eklendi

--- src/gitHubday02_1.java
+++ src/gitHubday02_1.java
@@ -2,6 +2,7 @@ public class gitHubday02_1 {
     public static void main(String[] args) {
 
         System.out.println("Merhaba tekrardan GitHub");
+        System.out.println("Ikinci satir ekleme islemi");
 
     }
 }
